6327db8a1a6a7bd74852c7e5418da6c684d5cb2f
unknown <=>
2022-05-17 13:54:00 +0500

    FirstCommit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from msilib.schema import ListView
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Post
+
+class PostPageView(ListView):
+    model = Post
+    template_name = "posts.html"
